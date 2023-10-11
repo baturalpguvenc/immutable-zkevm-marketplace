543a472f6367a4a3a484c6e10b288e747d660f2e
Frank Li <b439988l@gmail.com>
2023-10-11 15:59:44 +1100

    fix things
    Signed-off-by: Frank Li <b439988l@gmail.com>

--- pages/collections/[address].tsx
+++ pages/collections/[address].tsx
@@ -56,16 +56,17 @@ export default function NFTPage() {
 
         // Listings
         const lres = await orderbookSDK.listListings({
-          status: orderbook.OrderStatus.ACTIVE,
+          status: orderbook.OrderStatusName.ACTIVE,
           sellItemContractAddress: String(address),
         });
 
+        // TODO: display listings as 'my listings'
         let filteredListings: orderbook.Order[] = lres.result;
-        if (userAddress) {
-          filteredListings = lres.result.filter(
-            (l) => l.accountAddress !== userAddress!.toLowerCase()
-          );
-        }
+        // if (userAddress) {
+        //   filteredListings = lres.result.filter(
+        //     (l) => l.accountAddress !== userAddress!.toLowerCase()
+        //   );
+        // }
 
         setData({
           listings: filteredListings,
